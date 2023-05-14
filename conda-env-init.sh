66e7a9a7009dd30c60917e235d23749a3088ecf5
Jorge <jorge@192.168.1.12>
2023-05-14 09:25:38 -0500

    Add SH conda commands

---
+++ conda-env-init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Crear el entorno virtual
+conda create --name bigdata-env python=3.10
+
+# Activar el entorno virtual
+conda activate bigdata-env
+
+#instalar pandas
+conda install pandas
