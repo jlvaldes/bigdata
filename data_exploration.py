82fe6c2e0975520d10a8ec80b311ceaab3225d9f
Jorge <jorge@192.168.1.12>
2023-05-16 23:44:15 -0500

    Add util module

---
+++ data_exploration.py
@@ -0,0 +1,12 @@
+from global_variables import DB_HOST, DB_PASS, DB_PORT, DB_USER
+
+#exploración de correlacipon entre el tiempo de review y etc...
+def data_corr():
+    data = [12,34,56,78]
+    return 0
+
+
+
+
+if __name__ == "__main__":
+    data_corr()
