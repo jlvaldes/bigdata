785c2fec5f7a451320821061e9f8116cb5ae5569
SebasMora28 <juansem0r4@gmail.com>
2023-05-19 08:37:16 -0500

    Eliminación de columnas con links en books_data y transformación de la columna publishedDate

--- data_quality.py
+++ data_quality.py
@@ -12,8 +12,22 @@ def prepare_book_data(df: pd.DataFrame):
     df_cleaned = pd.DataFrame(df)
     columas_eliminar = ["image", "previewLink", "infoLink"]
     df_cleaned=df_cleaned.drop(columns=columas_eliminar, axis=1)
-    print(df_cleaned.dtypes)
-    df_cleaned.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
+    
+    print(df_cleaned["publishedDate"].isnull().sum())
+    
+    df_cleaned['publishedDate'].fillna(0, inplace=True)
+
+    def extract_year(value):
+        if value != 0 and value.isdigit() and len(value) >= 4:
+            return int(value[:4])
+        else:
+            return 0
+            
+    df_cleaned['publishedDate'] = df_cleaned['publishedDate'].apply(extract_year)
+    
+    print(df_cleaned["publishedDate"].value_counts())
+    
+    #df_cleaned.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
     return df_cleaned
 
 
@@ -28,7 +42,7 @@ def prepare_book_rating(df: pd.DataFrame):
     df_cleaned.to_csv(CSV_BOOK_RATING_CLEANED_PATH)
     return df_cleaned
 
-#df=pd.read_csv('../Datasets/books_data.csv')
-#prepare_book_data(df)
+df=pd.read_csv('../Datasets/books_data.csv')
+prepare_book_data(df)
 
 
