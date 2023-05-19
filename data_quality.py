c24eb4bf252e2ee5de6f9955f6f458a2922eb85d
SebasMora28 <juansem0r4@gmail.com>
2023-05-19 08:44:49 -0500

    Eliminación de columnas y transformación de datos

--- data_quality.py
+++ data_quality.py
@@ -13,20 +13,26 @@ def prepare_book_data(df: pd.DataFrame):
     columas_eliminar = ["image", "previewLink", "infoLink"]
     df_cleaned=df_cleaned.drop(columns=columas_eliminar, axis=1)
     
+    #Contar nulls
     print(df_cleaned["publishedDate"].isnull().sum())
     
+    #Reemplazar nulls por 0
     df_cleaned['publishedDate'].fillna(0, inplace=True)
-
+    
+    #Transformar datos solo a años
     def extract_year(value):
         if value != 0 and value.isdigit() and len(value) >= 4:
             return int(value[:4])
         else:
             return 0
             
+    #Llamar función
     df_cleaned['publishedDate'] = df_cleaned['publishedDate'].apply(extract_year)
     
+    #Contar valores
     print(df_cleaned["publishedDate"].value_counts())
     
+    #Retornar CSV
     #df_cleaned.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
     return df_cleaned
 
