4ef4d9056385642e9893cf75dafa93ed124acf1e
Daniel Ocampo <daniel.ocampo@cloudkitchens.com>
2023-05-18 22:22:05 -0500

    Actualización formula data_extraction y creacion de un notebook para hacer tests de script

--- conda-env-init.sh
+++ conda-env-init.sh
@@ -6,4 +6,4 @@ conda install -c conda-force python=3.10
 conda install -c conda-forge pandas
 conda install -c conda-forge prefect
 conda install -c conda-force ipykernel
-conda install -c conda-force sqlalchemy
+conda install -c conda-force sqlalchemyconda activate 
--- data_extraction.py
+++ data_extraction.py
@@ -10,25 +10,46 @@ def extract_book_data( percent = PERCENT):
     df = pd.DataFrame()
 
     #leer el csv original CSV_BOOK_DATA_PATH
+    df = pd.read_csv(CSV_BOOK_DATA_PATH)
     #eliminiar del df las columnas que no usamos
+    df_clean = df[[
+    'Title', 
+    'description', 
+    'authors', 
+    'publisher',
+    'publishedDate', 
+    'categories', 
+    'ratingsCount']]
     #almacenas ese df en un nuevo csv (localmente, el raiz del proyecto)
 
-    df.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
+    df_clean.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
 
-    return df
+    return df_clean
 
 
 #generar un nuevo CSV con la información de los libros, solo con las columnas de interés
 def extract_book_rating(percent = PERCENT):
     df = pd.DataFrame()
-    print("prueba")
+   
     #leer el csv original CSV_BOOK_RATING_PATH
+    df2 = pd.read_csv(CSV_BOOK_RATING_PATH)
     #eliminiar del df las columnas que no usamos
+    df_clean_rating = df2[[
+    'Id', 
+    'Title', 
+    'Price', 
+    'User_id', 
+    'profileName', 
+    'review/helpfulness',
+    'review/score', 
+    'review/time', 
+    'review/summary', 
+    'review/text']]
     #almacenas ese df en un nuevo csv (localmente, el raiz del proyecto)
 
-    df.to_csv(CSV_BOOK_RATING_CLEANED_PATH)
+    df_clean_rating.to_csv(CSV_BOOK_RATING_CLEANED_PATH)
 
-    return df
+    return df_clean_rating
 
 
 
