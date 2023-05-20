497fa0980902c3790bb460fa34492b6bd1d99ad1
SebasMora28 <juansem0r4@gmail.com>
2023-05-20 14:51:19 -0500

    Correción de transformación de la fecha y eliminación de caracteres

--- data_quality.py
+++ data_quality.py
@@ -13,27 +13,25 @@ def prepare_book_data(df: pd.DataFrame):
     columas_eliminar = ["image", "previewLink", "infoLink"]
     df_cleaned=df_cleaned.drop(columns=columas_eliminar, axis=1)
     
-    #Contar nulls
-    print(df_cleaned["publishedDate"].isnull().sum())
+    #Extaer año y convertir nulls en 0
+    df_cleaned['publishedDate'] = df_cleaned['publishedDate'].str.extract(r'(\d{4})')
+    df_cleaned['publishedDate'] = df_cleaned['publishedDate'].fillna('0')
     
-    #Reemplazar nulls por 0
-    df_cleaned['publishedDate'].fillna(0, inplace=True)
-    
-    #Transformar datos solo a años
-    def extract_year(value):
-        if value != 0 and value.isdigit() and len(value) >= 4:
-            return int(value[:4])
+    #Función para eliminar caracteres innecesarios
+    def quitar_corchetes(valor):
+        if isinstance(valor, str):
+            return valor.strip("[]''")
+        elif isinstance(valor, list):
+            return [v.strip("''") for v in valor]
         else:
-            return 0
-            
-    #Llamar función
-    df_cleaned['publishedDate'] = df_cleaned['publishedDate'].apply(extract_year)
-    
-    #Contar valores
-    print(df_cleaned["publishedDate"].value_counts())
+            return valor
+    #Eliminación de caracteres para authors y categories
+    df_cleaned['authors'] = df_cleaned['authors'].apply(quitar_corchetes)
+    df_cleaned['categories'] = df_cleaned['categories'].apply(quitar_corchetes)
     
     #Retornar CSV
-    #df_cleaned.to_csv(CSV_BOOK_DATA_CLEANED_PATH)
+    df_cleaned.to_csv(CSV_BOOK_DATA_CLEANED_PATH, index=False)
+    
     return df_cleaned
 
 
